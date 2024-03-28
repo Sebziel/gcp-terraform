a3a9662a286bd5a8175d3c8c02097dbf42d9740d
Sebziel <sebzie1996@gmail.com>
2024-03-28 09:56:52 +0000

    adjusting outputs

--- main.tf
+++ main.tf
@@ -12,22 +12,3 @@ module "kubernetesCluster" {
   region = var.region
 }
 
-output "region" {
-  value       = var.region
-  description = "GCloud Region"
-}
-
-output "project_id" {
-  value       = var.project_id
-  description = "GCloud Project ID"
-}
-
-output "kubernetes_custer_name" {
-  value = module.kubernetesCluster.kubernetes_cluster_name
-  description = "GKE cluster name"
-}
-
-output "kubernetes_cluster_host" {
-  value = module.kubernetesCluster.kubernetes_cluster_host
-  description = "Cluster Host"
-}
---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "region" {
+  value       = var.region
+  description = "GCloud Region"
+}
+
+output "project_id" {
+  value       = var.project_id
+  description = "GCloud Project ID"
+}
+
+output "kubernetes_cluster_name" {
+  value = module.kubernetesCluster.kubernetes_cluster_name
+  description = "GKE cluster name"
+}
+
+output "kubernetes_cluster_host" {
+  value = module.kubernetesCluster.kubernetes_cluster_host
+  description = "Cluster Host"
+}
