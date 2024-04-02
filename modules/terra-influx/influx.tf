7d6512fea4a5bc02f8f00799416b32e8b83634e9
Sebziel <sebzie1996@gmail.com>
2024-04-02 14:09:00 +0000

    started working on remote-exec provisioner for influx configuration

--- modules/terra-influx/influx.tf
+++ modules/terra-influx/influx.tf
@@ -1,3 +1,11 @@
+#Not recommended approach, forced by temporary service accounts created by ACG
+resource "null_resource" "generate_local_ssh_key" {
+  provisioner "local-exec" {
+    command = "ssh-keygen -t rsa -f ${path.module}/tmp_key -q -N ''"
+  }
+}
+
+#VM Instance definition
 resource "google_compute_instance" "influx-vm" {
   name         = "influx-instance"
   machine_type = "e2-medium"
@@ -18,6 +26,7 @@ resource "google_compute_instance" "influx-vm" {
       // Ephemeral public IP
     }
   }
-
+  #Unable to pass external IP as it requires for the resource to be finished first, can't 'self reference got to figure out a workoarund'
   metadata_startup_script = templatefile("${path.module}/influx-startup.sh.tftpl", { ipaddr = "123.123.123.123" , token ="sygoniummonstera"})
 }
+
